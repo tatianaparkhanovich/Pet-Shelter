6a236b178a5b6dc11eaaf270b787cc9a7f853b00
tatianaparkhanovich <tania.tigrovaa@gmail.com>
2023-07-18 21:27:35 +0300

    adding on background clicK

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const mobmenu = document.querySelector('.mob-menu');
 const dropdown = document.getElementById('myDropdown');
 mobmenu.addEventListener('click', (event) => {
-    if (dropdown.style.opacity === '0') {
+    if (dropdown.style.opacity === '0' || dropdown.style.opacity === "") {
         dropdown.style.opacity = '1';
         dropdown.style.visibility = 'visible';
         mobmenu.style.transform = 'rotate(90deg)';
     }  else {
         dropdown.style.opacity = '0';
         dropdown.style.visibility = 'hidden';
-    mobmenu.style.transform = 'rotate(0deg)'; 
+        mobmenu.style.transform = 'rotate(0deg)';    
     }
 })
 
-
+window.addEventListener('click', (event) => {
+    if (event.target.matches('.dropdown')) {
+        dropdown.style.opacity = '0';
+        dropdown.style.visibility = 'hidden';
+        mobmenu.style.transform = 'rotate(0deg)';    
+    }
+})
