882fbb96bfffb7cef1e1d32c9a57d2c6747dba1a
tatianaparkhanovich <tania.tigrovaa@gmail.com>
2023-07-20 00:01:26 +0300

    removed the scroll in the mobile menu fixed the body

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const mobmenu = document.querySelector('.mob-menu');
 const dropdown = document.getElementById('myDropdown');
+const body = document.querySelector('body')
 mobmenu.addEventListener('click', (event) => {
     if (dropdown.style.opacity === '0' || dropdown.style.opacity === "") {
         dropdown.style.opacity = '1';
         dropdown.style.visibility = 'visible';
+        body.style.position = 'fixed';
         mobmenu.style.transform = 'rotate(90deg)';
     }  else {
         dropdown.style.opacity = '0';
         dropdown.style.visibility = 'hidden';
+        body.style.position = 'static';
         mobmenu.style.transform = 'rotate(0deg)';    
     }
 })
