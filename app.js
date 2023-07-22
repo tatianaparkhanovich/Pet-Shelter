1183c7a0bc742cbe6545c38065bb05e9dcabaa72
tatianaparkhanovich <tania.tigrovaa@gmail.com>
2023-07-23 01:47:16 +0300

    added popup

--- app.js
+++ app.js
@@ -23,3 +23,22 @@ window.addEventListener('click', (event) => {
         mobmenu.style.transform = 'rotate(0deg)';    
     }
 })
+
+const popupBtns = document.querySelectorAll('.popup-btn');
+const mypopup = document.getElementById('mypopup');
+const popupClose = document.querySelector('.close');
+const popupContent = document.querySelector('.popup-content');
+
+popupBtns.forEach(item => {
+    item.addEventListener("click", (event) => {
+        mypopup.classList.add('visible');
+    })
+})
+popupClose.addEventListener("click", (event) => {
+    mypopup.classList.remove('visible');
+})
+window.addEventListener('click', (event) => {
+    if (event.target.matches('#mypopup')) {
+        mypopup.classList.remove('visible');
+    }
+})
